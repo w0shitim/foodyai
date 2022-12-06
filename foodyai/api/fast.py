cfc3d2264fc41e252dbdececdf0f079a40dafcee
timothee gidenne <tigid@protonmail.com>
2022-12-06 21:39:41 +0100

    first iteration API

---
+++ foodyai/api/fast.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from foodyai.ml_logic.mod_predict import prediction_setup, prediction, get_class_to_category, get_detectron_config
+
+app = FastAPI()
+#app.state.model =
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/predict")
+def predict(img_path: str):
