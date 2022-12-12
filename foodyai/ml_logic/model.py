7f6961afc8872cf2250ea05edd4faf81adcf12cc
timothee gidenne <tigid@protonmail.com>
2022-12-12 23:03:13 +0100

    add train() and evaluate() to main.py + commented all useless import + add save config to model_train() in model.py + added cfg to model_train() parameter in model.py

--- foodyai/ml_logic/model.py
+++ foodyai/ml_logic/model.py
@@ -1,5 +1,5 @@
 
-import detectron2
+#import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
@@ -51,9 +51,10 @@ def custom_config(training_dataset = ("training_dataset",),
 
 
 def model_train(output_dir = "logs/",
-                trainer_to_choose = DefaultTrainer):
+                trainer_to_choose = DefaultTrainer,
+                cfg=custom_config):
 
-    cfg = custom_config()
+    #cfg = custom_config()
 
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     cfg.OUTPUT_DIR = output_dir
@@ -61,6 +62,11 @@ def model_train(output_dir = "logs/",
     trainer = trainer_to_choose(cfg)
     trainer.resume_or_load(resume=False)
     trainer.train()
+
+    #save configuration of the model
+    f = open(os.path.join(output_dir,'config.yml'), 'w')
+    f.write(cfg.dump())
+    f.close()
 
     return cfg, trainer
 
@@ -82,8 +88,3 @@ def evaluate_model(validation_dataset = "validation_dataset",
     valResults = inference_on_dataset(trainer.model, val_loader, evaluator)
 
     return valResults, cfg, trainer
-
-if __name__ == '__main__':
-    custom_config()
-    model_train()
-    evaluate_model()
